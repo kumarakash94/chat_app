390896c3791fbbebc385517aea160f4734f1c243
Akash KUmar <kumar.ryan94@gmail.com>
2021-07-14 19:05:47 +0530

    initial commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,7 +2,7 @@ class SessionsController < ApplicationController
 	def new	
 	end
 	def create
-	user = User,find_by(username: params[:session][:username])
+	user = User.find_by(username: params[:session][:username])
 	if user && user.authenticate(params[:session][:password])
 		session[:user_id] = user.id
 		flash[:success] = "You logged in suceessfully."
